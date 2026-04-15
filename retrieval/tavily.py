92d7b5d909318541364770a00ca8038210f85f55
Rohitkar07 <112894965+Rohitkar07@users.noreply.github.com>
2026-04-16 02:45:56 +0530

    Update tavily.py

--- retrieval/tavily.py
+++ retrieval/tavily.py
@@ -1,12 +1,9 @@
 from tavily import TavilyClient
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+import streamlit as st
 
 class TavilySearch:
     def __init__(self):
-        self.client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+        self.client = TavilyClient(api_key=st.secrets["TAVILY_API_KEY"])
 
     def search(self, query, k=3):
         try:
